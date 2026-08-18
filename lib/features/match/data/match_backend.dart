6fd63db1d3f0b1038df47f73af5ddb87b6b472bf
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-18 10:22:22 +0300

    feat(match): define Blaze-ready backend boundary

---
+++ lib/features/match/data/match_backend.dart
@@ -0,0 +1,18 @@
+import '../../profile/domain/player_profile.dart';
+import '../domain/match_session.dart';
+import '../domain/match_ticket.dart';
+
+abstract class MatchBackend {
+  Future<void> joinQueue(PlayerProfile profile);
+
+  Future<void> leaveQueue(String uid);
+
+  Stream<MatchTicket?> watchTicket(String uid);
+
+  Stream<MatchSession?> watchMatch(String matchId);
+
+  Future<void> markReady({
+    required String matchId,
+    required String uid,
+  });
+}
