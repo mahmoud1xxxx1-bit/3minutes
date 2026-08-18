27c117b070d0da46414115fe10c6125b486c60f1
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-18 16:10:53 +0300

    feat: centralize social progression and season collections

--- lib/core/firebase/server_collections.dart
+++ lib/core/firebase/server_collections.dart
@@ -2,10 +2,28 @@ class ServerCollections {
   const ServerCollections._();
 
   static const seasons = 'seasons';
+  static const seasonPlayers = 'players';
+  static const seasonHistory = 'seasonHistory';
   static const leaderboards = 'leaderboards';
   static const leaderboardEntries = 'entries';
+
   static const inventories = 'inventories';
   static const inventoryItems = 'items';
-  static const rankedSettlements = 'rankedSettlements';
   static const coinTransactions = 'coinTransactions';
+  static const prestigeStarTransactions = 'prestigeStarTransactions';
+  static const purchaseReceipts = 'purchaseReceipts';
+
+  static const friendships = 'friendships';
+  static const friendCodes = 'friendCodes';
+  static const recentPlayers = 'recentPlayers';
+  static const privateRooms = 'privateRooms';
+  static const parties = 'parties';
+
+  static const achievements = 'achievements';
+  static const playerAchievements = 'playerAchievements';
+  static const missions = 'missions';
+  static const playerMissions = 'playerMissions';
+  static const seasonPass = 'seasonPass';
+
+  static const rankedSettlements = 'rankedSettlements';
 }
