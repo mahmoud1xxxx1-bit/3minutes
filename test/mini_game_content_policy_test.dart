74f646f716229af02d5ad7641ee3905f395a53d9
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-18 12:09:15 +0300

    test: require every registered mini-game to be playable

---
+++ test/mini_game_content_policy_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:game/features/minigames/domain/mini_game_content_policy.dart';
+
+void main() {
+  test('every registered mini-game has exactly one known engine', () {
+    expect(MiniGameContentPolicy.validate, returnsNormally);
+  });
+}
