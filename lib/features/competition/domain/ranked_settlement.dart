6ab6f9ed890b46f34df1c2a863fa754adb262bc6
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-18 12:16:31 +0300

    feat: serialize authoritative ranked settlement responses

--- lib/features/competition/domain/ranked_settlement.dart
+++ lib/features/competition/domain/ranked_settlement.dart
@@ -1,5 +1,13 @@
 import 'rank_tier.dart';
 
+RankTier _tierFromWire(Object? value) {
+  final name = value as String?;
+  for (final tier in RankTier.values) {
+    if (tier.name == name) return tier;
+  }
+  return RankTier.bronze;
+}
+
 class RankedPlayerSettlement {
   const RankedPlayerSettlement({
     required this.uid,
@@ -20,6 +28,30 @@ class RankedPlayerSettlement {
   final RankTier nextTier;
   final int xpAwarded;
   final int coinsAwarded;
+
+  Map<String, Object> toMap() => {
+        'uid': uid,
+        'previousRp': previousRp,
+        'nextRp': nextRp,
+        'rpDelta': rpDelta,
+        'previousTier': previousTier.name,
+        'nextTier': nextTier.name,
+        'xpAwarded': xpAwarded,
+        'coinsAwarded': coinsAwarded,
+      };
+
+  factory RankedPlayerSettlement.fromMap(Map<Object?, Object?> map) {
+    return RankedPlayerSettlement(
+      uid: map['uid'] as String? ?? '',
+      previousRp: (map['previousRp'] as num?)?.toInt() ?? 0,
+      nextRp: (map['nextRp'] as num?)?.toInt() ?? 0,
+      rpDelta: (map['rpDelta'] as num?)?.toInt() ?? 0,
+      previousTier: _tierFromWire(map['previousTier']),
+      nextTier: _tierFromWire(map['nextTier']),
+      xpAwarded: (map['xpAwarded'] as num?)?.toInt() ?? 0,
+      coinsAwarded: (map['coinsAwarded'] as num?)?.toInt() ?? 0,
+    );
+  }
 }
 
 class RankedMatchSettlement {
@@ -36,4 +68,35 @@ class RankedMatchSettlement {
   final RankedPlayerSettlement playerA;
   final RankedPlayerSettlement playerB;
   final DateTime settledAt;
+
+  Map<String, Object> toMap() => {
+        'matchId': matchId,
+        'seasonId': seasonId,
+        'playerA': playerA.toMap(),
+        'playerB': playerB.toMap(),
+        'settledAt': settledAt.toUtc().toIso8601String(),
+      };
+
+  factory RankedMatchSettlement.fromMap(Map<Object?, Object?> map) {
+    final rawPlayerA = map['playerA'];
+    final rawPlayerB = map['playerB'];
+    final rawSettledAt = map['settledAt'] as String?;
+
+    return RankedMatchSettlement(
+      matchId: map['matchId'] as String? ?? '',
+      seasonId: map['seasonId'] as String? ?? '',
+      playerA: RankedPlayerSettlement.fromMap(
+        rawPlayerA is Map
+            ? Map<Object?, Object?>.from(rawPlayerA)
+            : const <Object?, Object?>{},
+      ),
+      playerB: RankedPlayerSettlement.fromMap(
+        rawPlayerB is Map
+            ? Map<Object?, Object?>.from(rawPlayerB)
+            : const <Object?, Object?>{},
+      ),
+      settledAt: DateTime.tryParse(rawSettledAt ?? '')?.toUtc() ??
+          DateTime.fromMillisecondsSinceEpoch(0, isUtc: true),
+    );
+  }
 }
