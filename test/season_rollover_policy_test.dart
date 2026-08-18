0f0a081f505e3116a9ef9a543fc2f13a1ff712f8
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-18 17:16:07 +0300

    test: align diamond rollover with eight-tier reset

--- test/season_rollover_policy_test.dart
+++ test/season_rollover_policy_test.dart
@@ -11,6 +11,21 @@ void main() {
 
     expect(preview.starsAwarded, 11);
     expect(preview.nextPersistentStars, 31);
-    expect(preview.nextSeasonStartingRp, 2200);
+    expect(preview.nextSeasonStartingRp, 1400);
+  });
+
+  test('master retains a stronger head start than diamond', () {
+    final diamond = SeasonRolloverPolicy.preview(
+      currentPersistentStars: 0,
+      peakTier: RankTier.diamond,
+    );
+    final master = SeasonRolloverPolicy.preview(
+      currentPersistentStars: 0,
+      peakTier: RankTier.master,
+    );
+
+    expect(diamond.nextSeasonStartingRp, 1400);
+    expect(master.nextSeasonStartingRp, 2200);
+    expect(master.nextSeasonStartingRp, greaterThan(diamond.nextSeasonStartingRp));
   });
 }
