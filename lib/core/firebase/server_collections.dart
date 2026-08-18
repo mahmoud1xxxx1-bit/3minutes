b71537838c2627328ccd9e70cd7950f39285bd6c
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-18 11:29:06 +0300

    feat: centralize server-authoritative collection names

---
+++ lib/core/firebase/server_collections.dart
@@ -0,0 +1,11 @@
+class ServerCollections {
+  const ServerCollections._();
+
+  static const seasons = 'seasons';
+  static const leaderboards = 'leaderboards';
+  static const leaderboardEntries = 'entries';
+  static const inventories = 'inventories';
+  static const inventoryItems = 'items';
+  static const rankedSettlements = 'rankedSettlements';
+  static const coinTransactions = 'coinTransactions';
+}
