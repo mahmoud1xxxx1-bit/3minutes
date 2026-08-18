7c07819cbfc25c15f76a144866cfb00ffb89e7bd
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-18 11:28:19 +0300

    test: verify Spark backend phase gates trusted features

--- test/feature_flags_test.dart
+++ test/feature_flags_test.dart
@@ -3,6 +3,7 @@ import 'package:game/core/config/app_config.dart';
 
 void main() {
   test('server-authoritative features stay disabled on Spark', () {
+    expect(AppConfig.backendPhase, BackendPhase.spark);
     expect(AppConfig.rankedAuthorityEnabled, isFalse);
     expect(AppConfig.economyPurchasesEnabled, isFalse);
     expect(AppConfig.liveLeaderboardEnabled, isFalse);
