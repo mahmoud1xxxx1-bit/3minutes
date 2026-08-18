437492f0d2faf5e63a321b0dab8638db956a3bb4
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-18 09:34:40 +0300

    feat: add core game configuration

---
+++ lib/core/config/app_config.dart
@@ -0,0 +1,7 @@
+class AppConfig {
+  const AppConfig._();
+
+  static const String appName = '3 Minutes';
+  static const Duration matchDuration = Duration(minutes: 3);
+  static const int gamesPerMatch = 8;
+}
