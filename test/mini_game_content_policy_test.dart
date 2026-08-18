f228855079070d6d8d9605d74301aebeb80d74b2
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-18 12:09:44 +0300

    test: follow data-layer mini-game content policy

--- test/mini_game_content_policy_test.dart
+++ test/mini_game_content_policy_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:game/features/minigames/domain/mini_game_content_policy.dart';
+import 'package:game/features/minigames/data/mini_game_content_policy.dart';
 
 void main() {
   test('every registered mini-game has exactly one known engine', () {
