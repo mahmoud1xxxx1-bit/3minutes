948a5ba5243ed45338cce32d80c87fbdd23bcb66
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-19 06:00:29 +0300

    refactor(season): open missions from prominent season content

--- lib/features/competition/presentation/season_hub_screen.dart
+++ lib/features/competition/presentation/season_hub_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../../core/theme/design_tokens.dart';
 import '../../progression/data/progression_backend.dart';
-import '../../progression/presentation/progression_copy.dart';
 import '../../progression/presentation/progression_screen.dart';
 import '../data/competition_backend.dart';
 import 'season_screen.dart';
@@ -21,27 +19,17 @@ class SeasonHubScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final copy = ProgressionCopy.of(context);
-    return Stack(
-      children: [
-        SeasonScreen(competitionBackend: competitionBackend),
-        PositionedDirectional(
-          end: GameSpacing.md,
-          bottom: GameSpacing.md,
-          child: FloatingActionButton.extended(
-            heroTag: 'season-progression',
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute<void>(
-                  builder: (_) => ProgressionScreen(uid: uid, backend: progressionBackend),
-                ),
-              );
-            },
-            icon: const Icon(Icons.flag_circle_rounded),
-            label: Text(copy.missions),
-          ),
+    void openProgression() {
+      Navigator.of(context).push(
+        MaterialPageRoute<void>(
+          builder: (_) => ProgressionScreen(uid: uid, backend: progressionBackend),
         ),
-      ],
+      );
+    }
+
+    return SeasonScreen(
+      competitionBackend: competitionBackend,
+      onOpenMissions: openProgression,
     );
   }
 }
