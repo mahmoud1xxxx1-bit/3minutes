e56cee01eab05ebf9e4124e5fe6addaec4597262
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-19 08:14:55 +0300

    feat(season): add season history backend contract

--- lib/features/competition/data/competition_backend.dart
+++ lib/features/competition/data/competition_backend.dart
@@ -1,5 +1,6 @@
 import '../domain/leaderboard_entry.dart';
 import '../domain/season.dart';
+import '../domain/season_history.dart';
 
 abstract class CompetitionBackend {
   Stream<Season?> watchCurrentSeason();
@@ -7,4 +8,9 @@ abstract class CompetitionBackend {
   Future<List<LeaderboardEntry>> loadLeaderboard({int limit = 100});
 
   Stream<LeaderboardEntry?> watchPlayerCompetition(String uid);
+
+  Future<List<SeasonHistoryEntry>> loadSeasonHistory(
+    String uid, {
+    int limit = 12,
+  });
 }
