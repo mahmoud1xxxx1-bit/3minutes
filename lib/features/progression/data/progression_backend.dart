f23c304c89552ecd6d81338ae5f81da8a49e7098
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-19 09:07:19 +0300

    fix(progression): make mission and pass reads season aware

--- lib/features/progression/data/progression_backend.dart
+++ lib/features/progression/data/progression_backend.dart
@@ -5,12 +5,14 @@ enum SeasonPassClaimTrack { free, premium }
 
 class PlayerSeasonPassState {
   const PlayerSeasonPassState({
+    required this.seasonId,
     required this.seasonXp,
     required this.premiumUnlocked,
     required this.claimedFreeLevels,
     required this.claimedPremiumLevels,
   });
 
+  final String seasonId;
   final int seasonXp;
   final bool premiumUnlocked;
   final Set<int> claimedFreeLevels;
@@ -20,15 +22,25 @@ class PlayerSeasonPassState {
 abstract class ProgressionBackend {
   Stream<Map<String, PlayerAchievement>> watchAchievements(String uid);
 
-  Stream<Map<String, PlayerMissionState>> watchMissions(String uid);
+  Stream<Map<String, PlayerMissionState>> watchMissions(
+    String uid, {
+    required String seasonId,
+  });
 
-  Stream<PlayerSeasonPassState> watchSeasonPass(String uid);
+  Stream<PlayerSeasonPassState> watchSeasonPass(
+    String uid, {
+    required String seasonId,
+  });
 
-  Future<void> claimMissionReward(String missionId);
+  Future<void> claimMissionReward({
+    required String missionId,
+    required String seasonId,
+  });
 
   Future<void> claimAchievementReward(String achievementId);
 
   Future<void> claimSeasonPassReward({
+    required String seasonId,
     required int level,
     required SeasonPassClaimTrack track,
   });
