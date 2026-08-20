6244f27379393d5a4adaa5aac735bbd311a6359d
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-20 14:50:46 +0300

    test(find-differences): lock approved library v6

--- test/approved_game_library_test.dart
+++ test/approved_game_library_test.dart
@@ -2,10 +2,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:game/features/minigames/data/game_registry.dart';
 
 void main() {
-  test('approved mini-game library includes the three approved production games', () {
-    expect(GameRegistry.games.length, 13);
-    expect(GameRegistry.version, 5);
-    for (final id in ['mole_strike', 'follow_the_cup', 'path_rush']) {
+  test('approved mini-game library includes all four approved production games', () {
+    expect(GameRegistry.games.length, 14);
+    expect(GameRegistry.version, 6);
+    for (final id in [
+      'mole_strike',
+      'follow_the_cup',
+      'path_rush',
+      'find_differences',
+    ]) {
       expect(GameRegistry.games.where((game) => game.id == id).length, 1);
     }
   });
