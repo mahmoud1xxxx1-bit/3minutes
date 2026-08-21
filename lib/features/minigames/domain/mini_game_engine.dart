939cc92eb5f50ce1437f08f0bb667edec37944eb
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-21 11:45:13 +0300

    feat(minigames): register Find the Differences engine

--- lib/features/minigames/domain/mini_game_engine.dart
+++ lib/features/minigames/domain/mini_game_engine.dart
@@ -18,6 +18,7 @@ class MiniGameEngineRegistry {
     'path_rush': MiniGameEngine.choice,
     'tap_target': MiniGameEngine.target,
     'mole_strike': MiniGameEngine.target,
+    'find_differences': MiniGameEngine.target,
     'number_order': MiniGameEngine.sequence,
     'memory_flash': MiniGameEngine.sequence,
     'follow_the_cup': MiniGameEngine.sequence,
