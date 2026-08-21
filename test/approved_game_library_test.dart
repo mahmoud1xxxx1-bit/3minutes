eeb191547655342904a3755f899cbc30df164516
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-21 11:45:44 +0300

    test(minigames): approve HTML30 Find the Differences registration

--- test/approved_game_library_test.dart
+++ test/approved_game_library_test.dart
@@ -2,16 +2,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:game/features/minigames/data/game_registry.dart';
 
 void main() {
-  test('approved mini-game library excludes retired Find the Differences scenes', () {
-    expect(GameRegistry.games.length, 13);
-    expect(GameRegistry.version, 7);
+  test('approved mini-game library includes HTML30 Find the Differences', () {
+    expect(GameRegistry.games.length, 14);
+    expect(GameRegistry.version, 8);
     for (final id in [
       'mole_strike',
       'follow_the_cup',
       'path_rush',
+      'find_differences',
     ]) {
       expect(GameRegistry.games.where((game) => game.id == id).length, 1);
     }
-    expect(GameRegistry.games.where((game) => game.id == 'find_differences'), isEmpty);
   });
 }
