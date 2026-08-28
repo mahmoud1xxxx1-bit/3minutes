829b7aa20c99814b0416dd2bd1d86b0524da5630
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-29 00:56:22 +0300

    feat(server): export competitive forfeit

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -37,6 +37,7 @@ export {
   markCompetitiveReady,
 } from "./competitive.js";
 export { cancelCompetitiveMatch } from "./competitive_cancel.js";
+export { forfeitCompetitiveMatch } from "./competitive_forfeit.js";
 export { recoverCompetitiveQueue } from "./competitive_recovery.js";
 export { settleCompetitiveMatch } from "./competitive_settlement.js";
 
