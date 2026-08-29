99b89a0a1a51876bc7c557c63b8e9ff715221da3
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-29 11:32:08 +0300

    feat(preview): add Mole Strike source for first eight preview

---
+++ lib/features/minigames/presentation/mole_strike/mole_strike_game.dart
@@ -0,0 +1,8 @@
+import 'dart:math' as math;
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import '../../domain/mini_game_contract.dart';
+import '../shared/minigame_environment.dart';
+
+// NOTE: Direct source integration from supplied game batch.
