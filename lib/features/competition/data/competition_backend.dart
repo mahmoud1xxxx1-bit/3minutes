d0024110b9c962cbb63e9256a7d1e7fc621d3073
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-31 12:10:54 +0300

    feat(competition): expose weekly leaderboard loading

--- lib/features/competition/data/competition_backend.dart
+++ lib/features/competition/data/competition_backend.dart
@@ -1,11 +1,17 @@
 import '../domain/leaderboard_entry.dart';
 import '../domain/season.dart';
 import '../domain/season_history.dart';
+import '../domain/weekly_leaderboard_entry.dart';
 
 abstract class CompetitionBackend {
   Stream<Season?> watchCurrentSeason();
 
   Future<List<LeaderboardEntry>> loadLeaderboard({int limit = 100});
+
+  Future<List<WeeklyLeaderboardEntry>> loadWeeklyLeaderboard(
+    WeeklyLeaderboardKind kind, {
+    int limit = 100,
+  });
 
   Stream<LeaderboardEntry?> watchPlayerCompetition(String uid);
 
