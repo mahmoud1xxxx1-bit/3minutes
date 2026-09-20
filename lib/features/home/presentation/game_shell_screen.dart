505d20afa46e4089981f91479b8a70ee90579268
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-09-21 01:41:04 +0300

    ui: replace generic navigation with tactile game navigation

--- lib/features/home/presentation/game_shell_screen.dart
+++ lib/features/home/presentation/game_shell_screen.dart
@@ -7,6 +7,7 @@ import '../../../core/audio/game_audio_controller.dart';
 import '../../../core/platform/room_invite_service.dart';
 import '../../../core/theme/cosmic_background.dart';
 import '../../../core/theme/design_tokens.dart';
+import '../../../core/theme/game_bottom_nav.dart';
 import '../../../l10n/app_localizations.dart';
 import '../../auth/data/auth_service.dart';
 import '../../competition/data/competition_backend.dart';
@@ -163,78 +164,39 @@ class _GameShellScreenState extends State<GameShellScreen> {
           body: CosmicBackground(
             child: IndexedStack(index: _index, children: pages),
           ),
-          bottomNavigationBar: SafeArea(
-            top: false,
-            minimum: const EdgeInsets.fromLTRB(10, 0, 10, 8),
-            child: Container(
-              decoration: BoxDecoration(
-                color: GameColors.surfaceGlass,
-                borderRadius: BorderRadius.circular(22),
-                border: Border.all(
-                  color: GameColors.surfaceStrong,
-                  width: .8,
-                ),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Color(0x33000000),
-                    blurRadius: 24,
-                    offset: Offset(0, 10),
-                  ),
-                ],
+          bottomNavigationBar: GameBottomNav(
+            currentIndex: _index,
+            onSelected: (value) {
+              unawaited(GameAudioController.instance.playSfx(GameSfx.tap));
+              setState(() => _index = value);
+            },
+            items: [
+              GameBottomNavItem(
+                icon: Icons.home_outlined,
+                activeIcon: Icons.home_rounded,
+                label: l10n.home,
               ),
-              clipBehavior: Clip.antiAlias,
-              child: NavigationBar(
-                selectedIndex: _index,
-                onDestinationSelected: (value) {
-                  unawaited(GameAudioController.instance.playSfx(GameSfx.tap));
-                  setState(() => _index = value);
-                },
-                backgroundColor: Colors.transparent,
-                indicatorColor: GameColors.accentSoft,
-                destinations: [
-                  NavigationDestination(
-                    icon: const Icon(Icons.home_rounded),
-                    selectedIcon: const Icon(
-                      Icons.home_rounded,
-                      color: GameColors.accentBright,
-                    ),
-                    label: l10n.home,
-                  ),
-                  NavigationDestination(
-                    icon: const Icon(Icons.auto_awesome_rounded),
-                    selectedIcon: const Icon(
-                      Icons.auto_awesome_rounded,
-                      color: GameColors.accentBright,
-                    ),
-                    label: l10n.season,
-                  ),
-                  NavigationDestination(
-                    icon: const Icon(Icons.group_rounded),
-                    selectedIcon: const Icon(
-                      Icons.group_rounded,
-                      color: GameColors.accentBright,
-                    ),
-                    label: social.friends,
-                  ),
-                  NavigationDestination(
-                    icon: const Icon(Icons.storefront_rounded),
-                    selectedIcon: const Icon(
-                      Icons.storefront_rounded,
-                      color: GameColors.accentBright,
-                    ),
-                    label: l10n.shop,
-                  ),
-                  NavigationDestination(
-                    icon: const Icon(Icons.person_rounded),
-                    selectedIcon: const Icon(
-                      Icons.person_rounded,
-                      color: GameColors.accentBright,
-                    ),
-                    label: l10n.profile,
-                  ),
-                ],
+              GameBottomNavItem(
+                icon: Icons.auto_awesome_outlined,
+                activeIcon: Icons.auto_awesome_rounded,
+                label: l10n.season,
               ),
-            ),
+              GameBottomNavItem(
+                icon: Icons.group_outlined,
+                activeIcon: Icons.group_rounded,
+                label: social.friends,
+              ),
+              GameBottomNavItem(
+                icon: Icons.storefront_outlined,
+                activeIcon: Icons.storefront_rounded,
+                label: l10n.shop,
+              ),
+              GameBottomNavItem(
+                icon: Icons.person_outline_rounded,
+                activeIcon: Icons.person_rounded,
+                label: l10n.profile,
+              ),
+            ],
           ),
         );
       },
