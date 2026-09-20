9998853e9460d746d7fe63fa1e24346e9696bb13
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-09-21 02:32:33 +0300

    test: fix key escape smoke test configuration

--- lib/test_key_escape.dart
+++ lib/test_key_escape.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'features/minigames/presentation/key_escape/key_escape_game.dart';
-import 'package:flutter/material.dart';
-import 'features/minigames/presentation/key_escape/key_escape_game.dart';
 
 import 'features/minigames/domain/mini_game_contract.dart';
 
@@ -11,6 +9,7 @@ void main() {
     home: KeyEscapeGame(
       config: MiniGameConfig(
         seed: 1234,
+        difficulty: 1,
       ),
       onComplete: _ignoreResult,
     ),
