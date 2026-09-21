d4579c70db1c11d0d73091b0754afd35446f3eb2
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-09-21 06:56:25 +0300

    feat: prepare rewarded ads infrastructure

--- lib/main_lvllo_app.dart
+++ lib/main_lvllo_app.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import 'core/theme/app_theme.dart';
 import 'core/navigation/game_orientation.dart';
@@ -13,6 +14,7 @@ import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await MobileAds.instance.initialize();
 
   // Never block the first frame on Firebase or any remote/plugin startup.
   // The game must always render a real screen first.
