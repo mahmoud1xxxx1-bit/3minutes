86bd39b037bac8fc9410313420b15e60fb8a1748
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-09-22 11:44:24 +0300

    Point Flutter Firebase initialization to LVL LOOL Firebase

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,54 +1,29 @@
-
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      return web;
+      throw UnsupportedError(
+        'LVL LOOL Firebase is currently configured for Android only.',
+      );
     }
+
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return android;
-      case TargetPlatform.iOS:
-        return ios;
-      case TargetPlatform.macOS:
-        return macos;
-      case TargetPlatform.windows:
-        throw UnsupportedError('DefaultFirebaseOptions have not been configured for windows.');
-      case TargetPlatform.linux:
-        throw UnsupportedError('DefaultFirebaseOptions have not been configured for linux.');
       default:
-        throw UnsupportedError('DefaultFirebaseOptions are not supported for this platform.');
+        throw UnsupportedError(
+          'LVL LOOL Firebase is currently configured for Android only.',
+        );
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyBr386GNXJ2AQnrZNmS8qo7HBvbeCuDLII',
-    appId: '1:492486855771:web:55d81b88c7657f0883037e',
-    messagingSenderId: '492486855771',
-    projectId: 'minutes-d7dfc',
-    authDomain: 'minutes-d7dfc.firebaseapp.com',
-    storageBucket: 'minutes-d7dfc.firebasestorage.app',
-    measurementId: 'G-1Y66W3ZE28',
-  );
-
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyBr386GNXJ2AQnrZNmS8qo7HBvbeCuDLII', // Fallback, normally relies on google-services.json
-    appId: '1:492486855771:android:7cbe840f5695f19883037e', // From previous screenshot
-    messagingSenderId: '492486855771',
-    projectId: 'minutes-d7dfc',
-    storageBucket: 'minutes-d7dfc.firebasestorage.app',
+    apiKey: 'AIzaSyCAVxsqrde3eVBQhBBj-zx05UEZbPbCEdQ',
+    appId: '1:351650195477:android:ef05ef635311151e6d461c',
+    messagingSenderId: '351650195477',
+    projectId: 'lvl-lool',
+    storageBucket: 'lvl-lool.firebasestorage.app',
   );
-
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyBr386GNXJ2AQnrZNmS8qo7HBvbeCuDLII',
-    appId: '1:492486855771:ios:dummyiosappid',
-    messagingSenderId: '492486855771',
-    projectId: 'minutes-d7dfc',
-    storageBucket: 'minutes-d7dfc.firebasestorage.app',
-  );
-
-  static const FirebaseOptions macos = ios;
 }
-
