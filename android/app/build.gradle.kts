ff2d21766e3a16a0dae3b8ed439739a3aa699b10
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-09-22 11:45:28 +0300

    Remove legacy Android namespace from LVL LOOL

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.threeminutes.game"
+    namespace = "com.lvlool.game"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
